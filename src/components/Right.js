7f4d003e86a190011ed893a90cf1d3ad1e88ed3b
jaseel-koolath <112375897+jaseel-koolath@users.noreply.github.com>
2022-10-27 18:47:49 +0530

    Correction Delete typo

--- src/components/Right.js
+++ src/components/Right.js
@@ -4,7 +4,7 @@ import "./Right.css";
 function Right(props) {
   return (
     <div className="right">
-      <h4>Delelted</h4>
+      <h4>Deleted</h4>
       {props.list.map((elem) => (
         <p key={elem.id}>{elem.item}</p>
       ))}
